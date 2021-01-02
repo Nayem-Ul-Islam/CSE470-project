4f52856631de60de3c90f8c6509013ca8a18252c
Nayem-Ul-Islam <39053830+Nayem-Ul-Islam@users.noreply.github.com>
2021-01-02 21:18:15 +0600

    Add files via upload

---
+++ test/AirlinerTest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import controller.Airliner;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author USER
+ */
+public class AirlinerTest {
+    Airliner a;
+   @Before
+   public void setUp()throws Exception{
+       a = new Airliner();
+   }
+       
+   @Test
+   public void testgetCountriesOperated(){
+       assertEquals(0,a.getCountriesOperated());
+       
+   }
+   @Test
+   public void testsetCountriesOperated(){
+       a.setCountriesOperated(5);
+       assertEquals(5,a.getCountriesOperated());
+   }
+    @Test
+    public void testgetOriginCountry(){
+        assertEquals(null,a.getOriginCountry());
+        
+    }
+    @Test
+    public void testsetOriginCountry(){
+        a.setOriginCountry(null);
+        assertEquals(null,a.getOriginCountry());
+    }
+    @Test
+    public void testgetName(){
+        assertEquals(null,a.getName());
+    }
+    @Test
+    public void testsetName(){
+        a.setName("Nayem");
+        assertEquals("Nayem",a.getName());
+        
+    }
+    @Test
+    public void testgetAirlineCode(){
+        assertEquals(null,a.getAirlineCode());
+    }
+    @Test
+    public void testsetAirlineCode(){
+        a.setAirlineCode("A01");
+        assertEquals("A01",a.getAirlineCode());
+    }
+    @Test
+    public void testgetNumOfFlights(){
+        assertEquals(null,a.getAirlineCode());
+        
+    }
+    @Test
+    public void testsetNumOfFlights(){
+        a.setNumOfFlights(50);
+        assertEquals(null,a.getAirlineCode());
+        
+    }
+    @Test
+    public void testtoString(){
+        a.toString();
+        assertEquals(null,a.getName());
+        
+    }
+}
